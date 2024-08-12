82dc751987c2f74670cfb4aaca0162a245a457ed
sathishk <sathishkumar.thiyagarajan@gmail.com>
2024-08-12 14:23:18 +0530

    Green Chalo Done right 4

--- src/main/resources/db/upgrades/V2__matches.sql
+++ src/main/resources/db/upgrades/V2__matches.sql
@@ -1,8 +1,5 @@
-create table matches (
-    question_id UUID references question_choice (id) not null,
-    choice_id UUID references question_choice (id),
-    match_id UUID references question_choice (id) not null,
-    foreign key (question_id) references question (id),
-    choice_id references question_choice (id),
-    foreign key (match_id) references question_choice (id)
+CREATE TABLE matches (
+	question_id uuid NOT NULL,
+	choice_id uuid NULL,
+	match_id uuid NOT NULL
 );
